6250f6b840f164c179eceb302f66cb1ddd511f06
VitaliyLenin <vitaliylenin77@gmail.com>
2023-02-21 22:50:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,10 @@
 import HomePage from 'pages/HomePage/HomePage';
+import MoviesPage from 'pages/MoviesPage/MoviesPage';
+import MovieDetails from './MovieDetails/MovieDetails';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+
 import { Routes, Route } from 'react-router-dom';
 
 import Menu from './Menu/Menu';
@@ -9,6 +15,12 @@ export const App = () => {
       <Menu />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
