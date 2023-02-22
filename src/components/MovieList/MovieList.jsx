a6a054b9254c80aedc50336e5a70b88d2ae8d095
VitaliyLenin <vitaliylenin77@gmail.com>
2023-02-22 22:06:44 +0200

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const MovieList = ({ movies, location }) => {
   const elements = movies.map(({ id, title }) => (
     <li key={id}>
-      <Link state={{ from: location }} to={`/posts/${id}`}>
+      <Link state={{ from: location }} to={`/movies/${id}}`}>
         {title}
       </Link>
     </li>
